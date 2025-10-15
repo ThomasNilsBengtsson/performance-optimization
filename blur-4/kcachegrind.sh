222c8e7d4f33bbfa3ddadfa8c8a60847757d20e2
aleighh <aallii.alazawi@gmail.com>
2025-10-15 12:18:34 +0200

    blur4

---
+++ blur-4/kcachegrind.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Making..."
+make clean > /dev/null 2>&1
+make > /dev/null 2>&1
+
+images=("im1.ppm" "im2.ppm" "im3.ppm" "im4.ppm")
+
+for img in "${images[@]}"; do
+    output="output_${img}"
+    echo "Running blur on $img..."
+    echo "Iteration $i:"
+    valgrind --tool=callgrind ./blur_opt 15 "data/$img" "it1_Ofast/kcachegrind/$output" 2>&1 #| grep -E "Percent of CPU this job got|Elapsed \(wall clock\)|Maximum resident set size|Major \(requiring I/O\) page faults|Minor \(reclaiming a frame\) page faults|Voluntary context switches|Involuntary context switches|File system inputs|File system outputs|Page size|Exit status"
+    echo "-----------------------------------------"
+done
